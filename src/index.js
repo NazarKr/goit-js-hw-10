7801b7af72a002d4312cb42a47287d793420ba21
NazarKr <nazarok1990@gmail.com>
2022-12-02 15:30:25 +0200

    update zd10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchCountry: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+let formValue = '';
+
+refs.searchCountry.addEventListener('input', debounce(formInput, DEBOUNCE_DELAY));
+
+function formInput(e) {
+    e.preventDefault();
+    formValue = refs.searchCountry.value.trim();
+    if (formValue === '') {
+      clearRender();
+      return;
+    }
+
+
+    fetchCountries(formValue)
+      .then(countries => {
+        if (countries.length === 1) {
+          clearRender();
+          renderCountryList(countries);
+          renderCountryInfo(countries);
+        } else if (countries.length > 1 && countries.length <= 10) {
+          clearRender();
+          renderCountryList(countries);
+        } else if (countries.length > 10) {
+          clearRender();
+          Notify.info(
+            'Too many mathces found. Please enter a more spesific name',
+            { timeout: 100, cssAnimationDuration: 1000 }
+          );
+        }
+      })
+      .catch(catchError);
+};
+
+function renderCountryList(countries) {
+    const markup = countries.map(country => {
+        return `<li class="country-item">
+      <img class='country-img' src="${country.flags.svg}" alt="flag">
+      <p class="country-name">${country.name.official}</p>
+    </li>`;
+    }).join('');
+    refs.countryList.insertAdjacentHTML('beforeend', markup);
+}
+
+
+function renderCountryInfo(countries) {
+  const markup = countries
+    .map(country => {
+      return `<p class="info-text">Capital: <span class="value">${country.capital}</span></p>
+      <p class="info-text">Population: <span class="value">${country.population}</span></p>
+      <p class="info-text">languages: <span class="value">${langs}</span></p>`;
+    })
+    .join('');
+  refs.countryInfo.insertAdjacentHTML('beforeend', markup);
+};
+
+function clearRender() {
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
+}
+
+
+function catchError() {
+  clearRender();
+  Notify.failure('Oops, there is no country with that name');
+}
+console.log()
